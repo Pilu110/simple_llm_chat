9d99d26a2a92533a9977b4fce612714d8bb81bef
Istvan Vincze <istvan_vincze@epam.com>
2026-01-30 12:17:58 +0100

    change quetion, print more detailed answer

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ HEADERS = {
     "Content-Type": "application/json"
 }
 
-QUESTION = "Foglald össze röviden egy mondatban, mi az a gépi tanulás."
+QUESTION = "Mi Hayao Miyakazi véleménye az AI-ról? Rövid, pár mondatos választ szeretnék."
 
 MODEL = "openai/gpt-oss-120b"
 
@@ -17,22 +17,30 @@ PAYLOAD = {
     "messages" : [
         {"role":"user", "content": QUESTION}
     ],
-    "max_tokens": 200
+    "max_tokens": 1000
 }
 
 
 def main():
+    print('Kérdés:')
     print('=' * 100)
     print(QUESTION)
     print('=' * 100)
 
     response = requests.post(API_URL, headers=HEADERS, json=PAYLOAD)
 
-    #print(response.text) # -- kikommentelt debug
+    print('Debug server response:')
+    print(response.text)
 
     response.raise_for_status()
     response_json = response.json()
 
+    print('Érvelés:')
+    print('=' * 100)
+    print(response_json['choices'][0]['message']['reasoning'])
+    print('=' * 100)
+
+    print('Válasz:')
     print(response_json['choices'][0]['message']['content'])
 
 if __name__ == '__main__':
